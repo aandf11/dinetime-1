73e77aee3ab3e09d7881fea2e3d819e91717c3b6
Yeh Boom <gammmkimo@gmail.com>
2020-09-22 09:55:34 +0800

    initial spring

---
+++ dinetime/src/main/java/se452/group5/dinetime/DinetimeApplication.java
@@ -0,0 +1,13 @@
+package se452.group5.dinetime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinetimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinetimeApplication.class, args);
+	}
+
+}
---
+++ dinetime/src/test/java/se452/group5/dinetime/DinetimeApplicationTests.java
@@ -0,0 +1,13 @@
+package se452.group5.dinetime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinetimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
