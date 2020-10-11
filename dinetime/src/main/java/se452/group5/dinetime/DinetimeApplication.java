62fb926811dac6ae32c161b77d95d486f3a0930e
Yeh Boom <gammmkimo@gmail.com>
2020-10-11 19:35:07 +0800

    Connect database to h2

--- dinetime/src/main/java/se452/group5/dinetime/DinetimeApplication.java
+++ dinetime/src/main/java/se452/group5/dinetime/DinetimeApplication.java
@@ -4,16 +4,32 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+
 @SpringBootApplication
 public class DinetimeApplication {
+
+	@Value( "${app.greeting}" )
+    private String greeting;
+  
+    @Bean
+    public CommandLineRunner showMessage() {
+      return (args) -> {
+        System.out.println("begin encoded");
+        System.out.println(greeting);
+              System.out.println("end encoded");
+      };
+	} 
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(DinetimeApplication.class, args);
 	}
-	@Bean
-	public void samplerCustomersetterandgettertest(){
-		Customer c = new Customer();
-		c.setName("MARKY");
+	// @Bean
+	// public void samplerCustomersetterandgettertest(){
+	// 	Customer c = new Customer();
+	// 	c.setName("MARKY");
 		
-	}
+	// }
 }
