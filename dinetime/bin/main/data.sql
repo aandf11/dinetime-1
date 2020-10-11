0eb6d3ef9081cd598ddeff2050f8f24c4ff653a9
Yeh Boom <gammmkimo@gmail.com>
2020-10-12 02:33:56 +0800

    Add restaurants_location/table_availability/restaurants_detail/reservation_info/rate_info tables. And insert test values. Next step  will need to move them into persistence code.

--- dinetime/bin/main/data.sql
+++ dinetime/bin/main/data.sql
@@ -1,4 +1,43 @@
 
 INSERT INTO customer_accounts(user_id, nm, email) VALUES
- ('11101', 'Sheena Wyre', 'swyre0@un.org');
+    ('23494', 'KATE PARKER', 'KPARKER501@GMAIL.COM'),
+	('87155', 'MARIA TALLEY', 'MARIA_TALLEY@YAHOO.COM');
+
+
+INSERT INTO business_accounts(user_id, business_nm, email) VALUES
+	('21313', 'MAMAS LITTLE BAKERY', 'MAMASBAKERY@GMAIL.COM'),
+	('76521', 'ORGINAL PANCAKE HOUSE', 'OPH@YAHOO.COM'),
+	('97564', 'PETES CAFE', 'PETESCAFE@GMAIL.COM');
+
+INSERT INTO restaurants_location(location_id, business_nm, address, table_num) VALUES
+	('25870', 'MAMAS LITTLE BAKERY', '123 Main St. Chicago, IL', 4),
+	('95631', 'MAMAS LITTLE BAKERY', '721 Michigan Ave. Chicago, IL', 5),
+	('76313', 'ORGINAL PANCAKE HOUSE', '75 Clark Rd. Chicago, IL', 2),
+	('90839', 'PETES CAFE', '321 STATE ST. Chicago, IL', 3);
+
+INSERT INTO table_availability(table_id, location_id, available, seats,available_time, available_day) VALUES
+	('01321', '25870', '1', 2,'12:00','2020-01-02'),
+    ('02433', '25870', '1', 4,'13:00', '2020-02-05'),
+    ('03647', '25870', '1', 5,'14:00', '2020-01-02'),
+    ('04367', '25870', '1', 2,'15:00', '2020-01-02'),
+    ('05424', '95631', '1', 2,'13:00', '2020-02-05'),
+    ('33104', '95631', '1', 5,'14:00', '2020-02-05'),
+    ('87421', '76313', '1', 4,'20:00', '2020-01-03'),
+    ('16404', '90839', '1', 2,'21:00', '2020-03-09'),
+    ('86505', '90839', '1', 4,'17:00', '2020-02-05'),
+    ('76506', '90839', '1', 6,'18:00', '2020-02-06');
+
+
+INSERT INTO restaurants_detail( location_id, restaurants_type, average_rate) VALUES
+	('25870', 'BAKERY', 5),
+	('90839', 'CAFE', 4);
+
+
+INSERT INTO reservation_info(reservation_id, location_id, user_id, table_id,reservation_time,reservation_date) VALUES
+	('001', '25870', '23494','01321', '12:00', '2020-01-02'),
+	('002', '95631', '87155','02433', '13:00', '2020-02-05');
+
+INSERT INTO rate_info(rate_id, user_id,location_id,ratingDate)VALUES
+	('R001','01321','23494', '2020-01-02'),
+	('R002','02433','87155', '2020-02-05');
 
--- dinetime/bin/main/schema-h2.sql
+++ dinetime/bin/main/schema-h2.sql
@@ -1,16 +1,62 @@
-DROP TABLE IF EXISTS course_students;
-DROP TABLE IF EXISTS students;
-DROP TABLE IF EXISTS course;
+
 DROP TABLE IF EXISTS customer_accounts;
+DROP TABLE IF EXISTS business_accounts;
+DROP TABLE IF EXISTS restaurants_location;
 
 
 CREATE SEQUENCE hibernate_sequence START WITH 100 INCREMENT BY 1;
-
-
 
 CREATE TABLE customer_accounts(
     id int primary key auto_increment,
 	user_id VARCHAR(10),
 	nm VARCHAR(50),
 	email VARCHAR(50)
+);
+
+CREATE TABLE business_accounts(
+    id int primary key auto_increment,
+	user_id VARCHAR(10),
+	business_nm VARCHAR(50),
+	email VARCHAR(50)
+);
+
+CREATE TABLE restaurants_location(
+    id int primary key auto_increment,
+	location_id VARCHAR(10),
+	business_nm VARCHAR(50),
+	address VARCHAR(50),
+	table_num int
+);
+
+
+CREATE TABLE table_availability(
+	table_id VARCHAR(10) primary key,
+	location_id VARCHAR(10),
+	available BOOL,
+	seats int,
+	available_time varchar(10),
+    available_day DATE
+);
+
+CREATE TABLE restaurants_detail(
+	id int primary key auto_increment,
+	location_id VARCHAR(10),
+	restaurants_type VARCHAR(10),
+	average_rate int
+);
+
+CREATE TABLE reservation_info(
+	reservation_id VARCHAR(10) primary key,
+	location_id VARCHAR(10),
+	user_id VARCHAR(10),
+	table_id VARCHAR(10),
+	reservation_time VARCHAR(10),
+    reservation_date DATE
+);
+
+CREATE TABLE rate_info(
+	rate_id VARCHAR(10) NOT NULL PRIMARY KEY,
+	location_id VARCHAR(10),
+	user_id VARCHAR(10),
+	ratingDate date
 );
